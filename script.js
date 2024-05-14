1d2a89da1fd4720c7273429a96499d97e51379ce
Jindřich Pavlíček <117192302+jpavlicek00@users.noreply.github.com>
2024-05-15 01:03:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const question =document.querySelector(".question");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", removeButtons);
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('no-btn').offsetWidth) -85;
+    var y = Math.random() * (window.innerHeight - document.getElementById('no-btn').offsetHeight) -48;
+    document.getElementById('no-btn').style.left = `${x}px`;
+    document.getElementById('no-btn').style.top = `${y}px`;
+    
+}
+
+function removeButtons() {
+    yesBtn.remove();
+    noBtn.remove();
+}
+
+function nextPage() {
+    window.location.href ="yes.html"
+}
+
