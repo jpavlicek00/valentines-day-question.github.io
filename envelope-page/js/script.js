c5a711f1ca93e2867ed0be8cb851a613450816fd
Jindřich Pavlíček <117192302+jpavlicek00@users.noreply.github.com>
2025-02-08 17:08:42 +0100

    Envelope change

--- envelope-page/js/script.js
+++ envelope-page/js/script.js
@@ -1,26 +1,2 @@
 //LOGIKA OBÁLKY//
 
-
-
-$(document).ready(function () {
-    $('.valentines').click(function () {
-        console.log("script.js byl úspěšně načten!");
-        var card = $('.card');
-        var text = $('.text');
-        
-
-        // Otevření obálky (kontrola pozice)
-        if (!card.hasClass('open')) {
-            card.animate({ top: '-90px' }, 'slow', function () {
-                text.fadeIn('slow'); // Zobrazení textu
-            });
-            card.addClass('open');
-        } else {
-            card.animate({ top: '5px' }, 'slow', function () {
-                text.fadeOut('slow'); // Skrytí textu při zavření
-            });
-            card.removeClass('open');
-        }
-    });
-});
-
